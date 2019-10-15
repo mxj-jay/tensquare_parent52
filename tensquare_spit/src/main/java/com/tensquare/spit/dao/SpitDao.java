bc873b2a02dc467a2c58abd44ea2911051748997
59742 <597428285@qq.com>
2019-10-15 23:52:14 +0800

    Revert "Revert "添加spit模块，修改部分其他模块""

---
+++ tensquare_spit/src/main/java/com/tensquare/spit/dao/SpitDao.java
@@ -0,0 +1,8 @@
+package com.tensquare.spit.dao;
+
+import com.tensquare.spit.pojo.Spit;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface SpitDao extends MongoRepository<Spit, String>{
+
+}
