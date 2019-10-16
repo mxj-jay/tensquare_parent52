0bae3a015395f156cd5c5cb2b51ecb511d24cc61
mxj-jay <597428285@qq.com>
2019-10-16 11:21:14 +0800

    spit模块的部分功能实现

--- tensquare_spit/src/main/java/com/tensquare/spit/controller/SpitController.java
+++ tensquare_spit/src/main/java/com/tensquare/spit/controller/SpitController.java
@@ -2,9 +2,12 @@ package com.tensquare.spit.controller;
 
 import com.tensquare.spit.pojo.Spit;
 import com.tensquare.spit.service.SpitService;
+import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +21,9 @@ public class SpitController {
 
     @Autowired
     private SpitService spitService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @RequestMapping(method = RequestMethod.GET)
     public Result findAll() {
@@ -46,5 +52,23 @@ public class SpitController {
     public Result delete(@PathVariable("spitId") String spitId) {
         spitService.deleteById(spitId);
         return new Result(true, StatusCode.OK, "删除成功");
+    }
+
+    @RequestMapping(value = "/comment/{parentid}/{page}/{size}", method = RequestMethod.GET)
+    public Result findByParentId(@PathVariable("parentid") String parentid, @PathVariable("page") int page, @PathVariable("size") int size) {
+        Page<Spit> byParentid = spitService.findByParentid(parentid, page, size);
+        return new Result(true, StatusCode.OK, "查询成功", new PageResult<Spit>(byParentid.getTotalElements(), byParentid.getContent()));
+    }
+
+    @RequestMapping(value = "/thumbup/{spitid}", method = RequestMethod.PUT)
+    public Result thumbup(@PathVariable String spitid) {
+        // 配置不能重复点赞操作
+        String userId = "111";  // 锁定用户
+        if (redisTemplate.opsForValue().get("thumbup_" + userId) != null) {
+            return new Result(false, StatusCode.REPERROR, "不能重复点赞");
+        }
+        spitService.thumbup(spitid);
+        redisTemplate.opsForValue().set("thumbup_" + userId, 1);
+        return new Result(true, StatusCode.OK, "点赞成功");
     }
 }
--- tensquare_spit/src/main/java/com/tensquare/spit/service/SpitService.java
+++ tensquare_spit/src/main/java/com/tensquare/spit/service/SpitService.java
@@ -3,10 +3,19 @@ package com.tensquare.spit.service;
 import com.tensquare.spit.dao.SpitDao;
 import com.tensquare.spit.pojo.Spit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sun.security.provider.ConfigFile;
 import util.IdWorker;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -22,6 +31,9 @@ public class SpitService {
     @Autowired
     private IdWorker idWorker;
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     public List<Spit> findAll() {
         return spitDao.findAll();
     }
@@ -32,6 +44,24 @@ public class SpitService {
 
     public void save(Spit spit) {
         spit.set_id(idWorker.nextId() + "");
+        //发布日期   
+        spit.setPublishtime(new Date());
+        // 浏览量
+        spit.setVisits(0);
+        // 分享量
+        spit.setShare(0);
+        // 点赞数
+        spit.setThumbup(0);
+        // 回复数
+        spit.setComment(0);
+        // 状态   
+        spit.setState("1");
+        // 如果当前吐槽存在父节点,那么父节点的回复数需要+1 (给别人的评论下评论)
+        if (spit.getParentid() != null && !"".equals(spit.getParentid())) {
+            mongoTemplate.updateFirst(Query.query(Criteria.where("_id").is(spit.getParentid())),
+                    new Update().inc("comment", 1), "spit");
+        }
+
         spitDao.save(spit);
     }
 
@@ -43,4 +73,27 @@ public class SpitService {
         spitDao.deleteById(id);
     }
 
+    public Page<Spit> findByParentid(String parentId, int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return spitDao.findByParentid(parentId, pageable);
+    }
+
+    /**
+     * 点赞操作
+     *
+     * @param spitid
+     */
+    public void thumbup(String spitid) {
+//        // 方式一: 每次均访问数据库,效率低
+//        Spit spit = spitDao.findById(spitid).get();
+//        spit.setThumbup((spit.getThumbup() == null ? 0 : spit.getThumbup()) + 1);
+//        spitDao.save(spit);
+
+        // 方式二: 优化,使用原生MongoDB命令实现自增 --- db.spit.update({"_id":"1"}, {$inc:{thumbup:NumberInt(1}})
+        Query query = new Query();
+        query.addCriteria(Criteria.where("_id").is("1"));
+        Update update = new Update();
+        update.inc("thumbup", 1);
+        mongoTemplate.updateFirst(query, update, "spit");
+    }
 }
