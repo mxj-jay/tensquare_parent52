0ff3d55e3dda98a9b28d2cc906e400e3af961359
mxj-jay <597428285@qq.com>
2019-10-18 22:35:34 +0800

    user用户管理,sms短信模块的部分功能实现

--- tensquare_search/src/main/java/com/tensquare/search/service/ArticleService.java
+++ tensquare_search/src/main/java/com/tensquare/search/service/ArticleService.java
@@ -3,6 +3,9 @@ package com.tensquare.search.service;
 import com.tensquare.search.dao.ArticleDao;
 import com.tensquare.search.pojo.Article;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import util.IdWorker;
@@ -20,5 +23,10 @@ public class ArticleService {
 //        article.setId(idWorker.nextId()+"");
         // 使用默认id
         articleDao.save(article);
+    }
+
+    public Page<Article> findByKey(String key, int page, int size) {
+        Pageable pageable = PageRequest.of(page-1, size);
+        return articleDao.findByTitleOrContentLike(key, key, pageable);
     }
 }
